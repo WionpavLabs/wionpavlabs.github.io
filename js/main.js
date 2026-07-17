ef638fa9ca45af2716c07188a3589ee751ef139f
WionpavLabs <bcktyali1221@gmail.com>
2026-07-17 23:37:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+// ORTAK COMPONENT YÜKLEYİCİ
 
+async function loadComponent(id, file){
+
+    const element = document.getElementById(id);
+
+    if(!element) return;
+
+    const response = await fetch(file);
+
+    const html = await response.text();
+
+    element.innerHTML = html;
+
+}
+
+
+// Arka planı yükle
+
+loadComponent(
+    "background-container",
+    "components/background.html"
+);
