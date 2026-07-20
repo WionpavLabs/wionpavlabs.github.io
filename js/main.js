979e314563b584ee03a328ed900303ed8fabcf30
WionpavLabs <bcktyali1221@gmail.com>
2026-07-20 11:46:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -263,21 +263,7 @@ viewer.onclick = (e)=>{
     window.addEventListener("scroll", updateActiveMenu);
     window.addEventListener("load", updateActiveMenu);
 }
-document.addEventListener("click", (e)=>{
 
-    if(e.target.classList.contains("viewer-close")){
-
-        console.log("ÇARPIYA BASILDI");
-
-        const viewer = document.getElementById("imageViewer");
-
-        viewer.classList.remove("active");
-
-        console.log(viewer.className);
-
-    }
-
-});
 window.addEventListener("click", function(e){
 
     if(e.target.matches(".viewer-close")){
